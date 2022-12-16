58b8041ca5754266eac81aabe957b3ec132aebec
Sarah Teichman <teichs@uw.edu>
2022-12-15 16:33:06 -0800

    updates to code for prevotella analysis

--- scripts/R/prevotella_analysis.R
+++ scripts/R/prevotella_analysis.R
@@ -38,7 +38,7 @@ plot_res <- plot_logmap(vectors = lm_res$vectors, phylogenomic = 64,
 plot_res$plot + 
   theme_bw() + 
   theme(plot.title = element_text(hjust = 0.5))
-ggsave("figures/prevotella/lm_viz.png")
+ggsave("figures/prevotella/lm_viz.jpeg")
 
 # compute proportion of variance explained by each component 
 pca_res <- stats::prcomp(lm_res$vectors)
@@ -57,7 +57,7 @@ plot_res <- plot_logmap(vectors = new_vectors, phylogenomic = 64, other_tree = 6
 plot_res$plot + 
   theme_bw() + 
   theme(plot.title = element_text(hjust = 0.5))
-ggsave("figures/prevotella/lm_viz_two_phy.png")
+ggsave("figures/prevotella/lm_viz_two_phy.jpeg")
 
 # next, look at individual gene trees 
 # we can see 3 clear outliers in the log map visualization 
@@ -73,7 +73,7 @@ plot_res <- plot_logmap(vectors = new_vectors, phylogenomic = 64, other_tree = 6
 plot_res$plot + 
   theme_bw() + 
   theme(plot.title = element_text(hjust = 0.5))
-ggsave("figures/prevotella/lm_viz_labeled.png")
+ggsave("figures/prevotella/lm_viz_labeled.jpeg")
 
 # plot trees 
 # phylogenomic tree 
@@ -86,7 +86,7 @@ phylogenomic_num_mid <- phytools::midpoint.root(phylogenomic_num)
 ggtree(phylogenomic_num_mid) + geom_tiplab(size = 2) + 
   xlim(c(0, 0.55)) + ggtitle("Phylogenomic Tree") + 
   theme(plot.title = element_text(hjust = 0.5))
-ggsave("figures/prevotella/phylogenomic_tree.png")
+ggsave("figures/prevotella/phylogenomic_tree.jpeg")
 # BacA gene tree 
 BacA <- read.tree("data/prevotella/gene_trees/BacA.txt")
 BacA_num <- rename_labs(match_df, BacA)
@@ -94,7 +94,7 @@ BacA_num_mid <- phytools::midpoint.root(BacA_num)
 ggtree(BacA_num_mid) + geom_tiplab(size = 2) + 
   xlim(c(0, 3.3)) + ggtitle("BacA Tree") + 
   theme(plot.title = element_text(hjust = 0.5))
-ggsave("figures/prevotella/BacA_tree.png")
+ggsave("figures/prevotella/BacA_tree.jpeg")
 # DMRL_synthase
 DMRL_synthase <- read.tree("data/prevotella/gene_trees/DMRL_synthase.txt")
 DMRL_synthase_num <- rename_labs(match_df, DMRL_synthase)
@@ -102,7 +102,7 @@ DMRL_synthase_num_mid <- phytools::midpoint.root(DMRL_synthase_num)
 ggtree(DMRL_synthase_num_mid) + geom_tiplab(size = 2) + 
   xlim(c(0, 6)) + ggtitle("DMRL_synthase Tree") + 
   theme(plot.title = element_text(hjust = 0.5))
-ggsave("figures/prevotella/DMRL_synthase.png")
+ggsave("figures/prevotella/DMRL_synthase.jpeg")
 # GTP_cyclohydroI 
 GTP_cyclohydroI <- read.tree("data/prevotella/gene_trees/GTP_cyclohydroI.txt")
 GTP_cyclohydroI_num <- rename_labs(match_df, GTP_cyclohydroI)
@@ -110,7 +110,7 @@ GTP_cyclohydroI_num_mid <- phytools::midpoint.root(GTP_cyclohydroI_num)
 ggtree(GTP_cyclohydroI_num_mid) + geom_tiplab(size = 2) + 
   xlim(c(0, 2.8)) + ggtitle("GTP_cyclohydroI Tree") + 
   theme(plot.title = element_text(hjust = 0.5))
-ggsave("figures/prevotella/GTP_cyclohydroI.png")
+ggsave("figures/prevotella/GTP_cyclohydroI.jpeg")
 
 # Plot gene tree support values 
 merge_txt(paths[1:63], "data/prevotella/gene_trees.txt")
@@ -120,7 +120,7 @@ support <- check_gene_support(main_tree = phylogenomic,
                   rooted = FALSE) 
 plot_support(phylogenomic_num, support, color_branch = TRUE,
              title = "Phylogenomic Tree Gene Support", xlim_max = 1.1)
-ggsave("figures/prevotella/gene_tree_support.png")
+ggsave("figures/prevotella/gene_tree_support.jpeg")
 
 # try with midpoint rooted tree 
 gene_trees_mid <- phangorn::midpoint(rename_labs(match_df, gene_trees))
@@ -129,7 +129,7 @@ support_mid <- check_gene_support(main_tree = phylogenomic_num_mid,
                                   rooted = TRUE)
 plot_support(phylogenomic_num_mid, support_mid, color_branch = TRUE,
              title = "Phylogenomic Tree Gene Support", xlim_max = 0.54)
-ggsave("figures/prevotella/gene_tree_support.png")
+ggsave("figures/prevotella/gene_tree_support.jpeg")
 
 # Plot bootstrap support values 
 phylo_bs <- phangorn::midpoint(rename_labs(match_df, 
@@ -139,7 +139,7 @@ boot[1] <- 1
 plot_support(phylo_bs, boot, color_branch = TRUE, 
              title = "Phylogenomic Bootstrap Support", xlim_max = 0.54,
              support_type = "boot")
-ggsave("figures/prevotella/bootstrap_support.png")
+ggsave("figures/prevotella/bootstrap_support.jpeg")
 
 # make table to match tip number to accession number to taxonomic data 
 taxonomic_info <- read.table("data/prevotella/NCBI_genomes_summary_info.tsv", 
@@ -176,7 +176,7 @@ bhv_dists <- compute_geodesic("data/prevotella/all_trees.txt")
 bhv_dist_vec <- bhv_dists[lower.tri(bhv_dists)]
 phy_bhv <- bhv_dists[nrow(bhv_dists), nrow(bhv_dists) - 1]
 mean(bhv_dist_vec >= phy_bhv)
-# 75% of BHV distances between trees are larger than the distance between the two 
+# 100% of BHV distances between trees are larger than the distance between the two 
 # phylogenomic trees
 # compare RF distances between all trees
 rf_dists <- as.matrix(phangorn::RF.dist(all_trees))
@@ -192,7 +192,7 @@ branch_sum_dists <- as.matrix(stats::dist(data.frame(x = unlist(branch_sums)),
 branch_sum_dist_vec <- branch_sum_dists[lower.tri(branch_sum_dists)]
 phy_branch_sum <- branch_sum_dists[nrow(branch_sum_dists), nrow(branch_sum_dists) - 1]
 mean(branch_sum_dist_vec >= phy_branch_sum)
-# 38% of squared branch sum differences are larger than the squared branch sum
+# 99% of squared branch sum differences are larger than the squared branch sum
 # difference between the two phylogenomic trees
 
 # check relative weight of branch to tip 51 in PC1 
